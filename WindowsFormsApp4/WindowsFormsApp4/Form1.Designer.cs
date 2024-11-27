619486f4e8d4b96f55244fae519ed13f07d7d887
stefanSCD <stefan.dobrincu@yahoo.com>
2024-11-27 08:24:51 +0200

    TicTacToe

---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lab1 = new System.Windows.Forms.Label();
+            this.lab2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 86);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(163, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 86);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(297, 31);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 86);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(25, 147);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 86);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(163, 147);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(93, 86);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(297, 147);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(93, 86);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(25, 263);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(93, 86);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(163, 263);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(93, 86);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(297, 263);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(93, 86);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mistral", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(319, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 44);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "TIC TAC TOE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Papyrus", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label2.Location = new System.Drawing.Point(12, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 30);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "JUCATOR O";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Papyrus", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label3.Location = new System.Drawing.Point(621, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 30);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "JUCATOR X";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(184, 76);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(414, 362);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            // 
+            // lab1
+            // 
+            this.lab1.AutoSize = true;
+            this.lab1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab1.Location = new System.Drawing.Point(53, 120);
+            this.lab1.Name = "lab1";
+            this.lab1.Size = new System.Drawing.Size(69, 73);
+            this.lab1.TabIndex = 13;
+            this.lab1.Text = "0";
+            // 
+            // lab2
+            // 
+            this.lab2.AutoSize = true;
+            this.lab2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab2.Location = new System.Drawing.Point(665, 120);
+            this.lab2.Name = "lab2";
+            this.lab2.Size = new System.Drawing.Size(69, 73);
+            this.lab2.TabIndex = 14;
+            this.lab2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lab2);
+            this.Controls.Add(this.lab1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lab1;
+        private System.Windows.Forms.Label lab2;
+    }
+}
+
---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,440 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int x = 1;
+        public int scor1 = 0, scor2 = 0;
+        int cnt = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cnt++;
+            if (x == 1)
+            {
+                this.button1.Text = "X";
+                x = 2;
+            }
+            else
+            {
+                this.button1.Text = "O";
+                x = 1;
+            }
+            this.button1.Enabled = false;
+            if (VerificaCastigator())
+            {
+                MessageBox.Show("Jucătorul " + (x == 1 ? "O" : "X") + " a câștigat!");
+                if (x == 1)
+                {
+                    scor1 += 1;
+                    this.lab1.Text = scor1.ToString();
+                }
+                else
+                {
+                    scor2 += 1;
+                    this.lab2.Text = scor2.ToString();
+                }
+                Resetare();
+                cnt = 0;
+            }
+            else if (cnt == 9)
+            {
+                MessageBox.Show("EGALITATE!");
+                Resetare();
+                cnt = 0;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cnt++;
+            if (x == 1)
+            {
+                this.button2.Text = "X";
+                x = 2;
+            }
+            else
+            {
+                this.button2.Text = "O";
+                x = 1;
+            }
+            this.button2.Enabled = false;
+            if (VerificaCastigator())
+            {
+                MessageBox.Show("Jucătorul " + (x == 1 ? "O" : "X") + " a câștigat!");
+                if (x == 1)
+                {
+                    scor1 += 1;
+                    this.lab1.Text = scor1.ToString();
+                }
+                else
+                {
+                    scor2 += 1;
+                    this.lab2.Text = scor2.ToString();
+                }
+                Resetare();
+                cnt = 0;
+            }
+            else if (cnt == 9)
+            {
+                MessageBox.Show("EGALITATE!");
+                Resetare();
+                cnt = 0;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            cnt++;
+            if (x == 1)
+            {
+                this.button3.Text = "X";
+                x = 2;
+            }
+            else
+            {
+                this.button3.Text = "O";
+                x = 1;
+            }
+            this.button3.Enabled = false;
+            if (VerificaCastigator())
+            {
+                MessageBox.Show("Jucătorul " + (x == 1 ? "O" : "X") + " a câștigat!");
+                if (x == 1)
+                {
+                    scor1 += 1;
+                    this.lab1.Text = scor1.ToString();
+                }
+                else
+                {
+                    scor2 += 1;
+                    this.lab2.Text = scor2.ToString();
+                }
+                Resetare();
+                cnt = 0;
+            }
+            else if (cnt == 9)
+            {
+                MessageBox.Show("EGALITATE!");
+                Resetare();
+                cnt = 0;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            cnt++;
+            if (x == 1)
+            {
+                this.button4.Text = "X";
+                x = 2;
+            }
+            else
+            {
+                this.button4.Text = "O";
+                x = 1;
+            }
+            this.button4.Enabled = false;
+            if (VerificaCastigator())
+            {
+                MessageBox.Show("Jucătorul " + (x == 1 ? "O" : "X") + " a câștigat!");
+                if (x == 1)
+                {
+                    scor1 += 1;
+                    this.lab1.Text = scor1.ToString();
+                }
+                else
+                {
+                    scor2 += 1;
+                    this.lab2.Text = scor2.ToString();
+                }
+                Resetare();
+                cnt = 0;
+            }
+            else if (cnt == 9)
+            {
+                MessageBox.Show("EGALITATE!");
+                Resetare();
+                cnt = 0;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            cnt++;
+            if (x == 1)
+            {
+                this.button5.Text = "X";
+                x = 2;
+            }
+            else
+            {
+                this.button5.Text = "O";
+                x = 1;
+            }
+            this.button5.Enabled = false;
+            if (VerificaCastigator())
+            {
+                MessageBox.Show("Jucătorul " + (x == 1 ? "O" : "X") + " a câștigat!");
+                if (x == 1)
+                {
+                    scor1 += 1;
+                    this.lab1.Text = scor1.ToString();
+                }
+                else
+                {
+                    scor2 += 1;
+                    this.lab2.Text = scor2.ToString();
+                }
+                Resetare();
+                cnt = 0;
+            }
+            else if (cnt == 9)
+            {
+                MessageBox.Show("EGALITATE!");
+                Resetare();
+                cnt = 0;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            cnt++;
+            if (x == 1)
+            {
+                this.button6.Text = "X";
+                x = 2;
+            }
+            else
+            {
+                this.button6.Text = "O";
+                x = 1;
+            }
+            this.button6.Enabled = false;
+            if (VerificaCastigator())
+            {
+                MessageBox.Show("Jucătorul " + (x == 1 ? "O" : "X") + " a câștigat!");
+                if (x == 1)
+                {
+                    scor1 += 1;
+                    this.lab1.Text = scor1.ToString();
+                }
+                else
+                {
+                    scor2 += 1;
+                    this.lab2.Text = scor2.ToString();
+                }
+                Resetare();
+                cnt = 0;
+            }
+            else if (cnt == 9)
+            {
+                MessageBox.Show("EGALITATE!");
+                Resetare();
+                cnt = 0;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            cnt++;
+            if (x == 1)
+            {
+                this.button7.Text = "X";
+                x = 2;
+            }
+            else
+            {
+                this.button7.Text = "O";
+                x = 1;
+            }
+            this.button7.Enabled = false;
+            if (VerificaCastigator())
+            {
+                MessageBox.Show("Jucătorul " + (x == 1 ? "O" : "X") + " a câștigat!");
+                if (x == 1)
+                {
+                    scor1 += 1;
+                    this.lab1.Text = scor1.ToString();
+                }
+                else
+                {
+                    scor2 += 1;
+                    this.lab2.Text = scor2.ToString();
+                }
+                Resetare();
+                cnt = 0;
+            }
+            else if (cnt == 9)
+            {
+                MessageBox.Show("EGALITATE!");
+                Resetare();
+                cnt = 0;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            cnt++;
+            if (x == 1)
+            {
+                this.button8.Text = "X";
+                x = 2;
+            }
+            else
+            {
+                this.button8.Text = "O";
+                x = 1;
+            }
+            this.button8.Enabled = false;
+            if (VerificaCastigator())
+            {
+                MessageBox.Show("Jucătorul " + (x == 1 ? "O" : "X") + " a câștigat!");
+                if (x == 1)
+                {
+                    scor1 += 1;
+                    this.lab1.Text = scor1.ToString();
+                }
+                else
+                {
+                    scor2 += 1;
+                    this.lab2.Text = scor2.ToString();
+                }
+                Resetare();
+                cnt = 0;
+            }
+            else if (cnt == 9)
+            {
+                MessageBox.Show("EGALITATE!");
+                Resetare();
+                cnt = 0;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            cnt++;
+            if (x == 1)
+            {
+                this.button9.Text = "X";
+                x = 2;
+            }
+            else
+            {
+                this.button9.Text = "O";
+                x = 1;
+            }
+            this.button9.Enabled = false;
+            if (VerificaCastigator())
+            {
+                MessageBox.Show("Jucătorul " + (x == 1 ? "O" : "X") + " a câștigat!");
+                if (x == 1)
+                {
+                    scor1 += 1;
+                    this.lab1.Text = scor1.ToString();
+                }
+                else
+                {
+                    scor2 += 1;
+                    this.lab2.Text = scor2.ToString();
+                }
+                Resetare();
+                cnt = 0;
+            }
+            else if (cnt == 9)
+            {
+                MessageBox.Show("EGALITATE!");
+                Resetare();
+                cnt = 0;
+            }
+        }
+        private void Resetare()
+        {
+            this.button1.Enabled = true;
+            this.button1.Text = "";
+            this.button2.Enabled = true;
+            this.button2.Text = "";
+            this.button3.Enabled = true;
+            this.button3.Text = "";
+            this.button4.Enabled = true;
+            this.button4.Text = "";
+            this.button5.Enabled = true;
+            this.button5.Text = "";
+            this.button6.Enabled = true;
+            this.button6.Text = "";
+            this.button7.Enabled = true;
+            this.button7.Text = "";
+            this.button8.Enabled = true;
+            this.button8.Text = "";
+            this.button9.Enabled = true;
+            this.button9.Text = "";
+        }
+        private bool VerificaCastigator()
+        {
+            // Verificare linii
+            for (int i = 0; i < 3; i++)
+            {
+                if (VerificaLinie(i))
+                    return true;
+            }
+
+            // Verificare coloane
+            for (int i = 0; i < 3; i++)
+            {
+                if (VerificaColoana(i))
+                    return true;
+            }
+
+            // Verificare diagonale
+            if (VerificaDiagonalaPrincipala() || VerificaDiagonalaSecundara())
+                return true;
+
+            return false;
+        }
+
+        private bool VerificaLinie(int linie)
+        {
+            return (GetSimbol(linie, 0) == GetSimbol(linie, 1) && GetSimbol(linie, 1) == GetSimbol(linie, 2) && GetSimbol(linie, 0) != "");
+        }
+
+        private bool VerificaColoana(int coloana)
+        {
+            return (GetSimbol(0, coloana) == GetSimbol(1, coloana) && GetSimbol(1, coloana) == GetSimbol(2, coloana) && GetSimbol(0, coloana) != "");
+        }
+
+        private bool VerificaDiagonalaPrincipala()
+        {
+            return (GetSimbol(0, 0) == GetSimbol(1, 1) && GetSimbol(1, 1) == GetSimbol(2, 2) && GetSimbol(0, 0) != "");
+        }
+
+        private bool VerificaDiagonalaSecundara()
+        {
+            return (GetSimbol(0, 2) == GetSimbol(1, 1) && GetSimbol(1, 1) == GetSimbol(2, 0) && GetSimbol(0, 2) != "");
+        }
+
+        private string GetSimbol(int linie, int coloana)
+        {
+            Control[] controls = this.Controls.Find("button" + (linie * 3 + coloana + 1), true);
+            if (controls.Length > 0 && controls[0] is Button)
+            {
+                return ((Button)controls[0]).Text;
+            }
+            return "";
+        }
+
+    }
+}
